69835fd1dc5d16427421a6833c8f521f94c13ee5
Harry Mander <harry.mander@pg.canterbury.ac.nz>
2026-05-04 12:32:38 +1200

    Tidy toolchain file

--- cmake/toolchains/sam4s.cmake
+++ cmake/toolchains/sam4s.cmake
@@ -1,24 +1,32 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-find_program(CMAKE_C_COMPILER arm-none-eabi-gcc REQUIRED)
-find_program(CMAKE_ASM_COMPILER arm-none-eabi-gcc REQUIRED)
-find_program(CMAKE_CXX_COMPILER arm-none-eabi-g++ REQUIRED)
-find_program(CMAKE_AS arm-none-eabi-as REQUIRED)
-find_program(CMAKE_AR arm-none-eabi-ar REQUIRED)
-find_program(CMAKE_OBJCOPY arm-none-eabi-objcopy REQUIRED)
-find_program(CMAKE_OBJDUMP arm-none-eabi-objdump REQUIRED)
-find_program(CMAKE_SIZE arm-none-eabi-size REQUIRED)
+set(triple arm-none-eabi)
+
+find_program(CMAKE_C_COMPILER ${triple}-gcc REQUIRED)
+find_program(CMAKE_ASM_COMPILER ${triple}-gcc REQUIRED)
+find_program(CMAKE_CXX_COMPILER ${triple}-g++ REQUIRED)
+find_program(CMAKE_AR ${triple}-ar REQUIRED)
+find_program(CMAKE_OBJCOPY ${triple}-objcopy REQUIRED)
+find_program(CMAKE_OBJDUMP ${triple}-objdump REQUIRED)
 
 set(
-    CMAKE_C_FLAGS
+    common_flags
     "-mcpu=cortex-m4 \
     -mthumb \
     -specs=nano.specs \
     -specs=nosys.specs \
     -DARM_MATH_CM4=true \
-    -Wstrict-prototypes \
     -mno-unaligned-access"
-    CACHE INTERNAL "C compiler flags"
 )
-set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS}" CACHE INTERNAL "C++ compiler flags")
+
+set(
+    CMAKE_C_FLAGS_INIT
+    "${common_flags} -Wstrict-prototypes"
+    CACHE INTERNAL "Initial C compiler flags"
+)
+set(
+    CMAKE_CXX_FLAGS_INIT
+    "${common_flags}"
+    CACHE INTERNAL "Initial C++ compiler flags"
+)
